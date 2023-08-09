aa45cff0b6c416b3e1bd307f12f8c475432c3762
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-08-09 16:15:35 -0700

    Added timer implementation and reset button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,14 @@
-
+var startButton = document.querySelector(".start-button");
+var timerEleValue = document.querySelector(".timer-count");
+var resetButton = document.querySelector(".reset-button");
+resetButton.disabled = true;
 
 //initialize timer to 60 seconds
 var timerCount = 60;
 //initialize win and lose to false and totals to zero
 var win = false;
 var lose = false;
+var reset = false;
 var totalWins = 0;
 var totalLoses = 0;
 
@@ -14,22 +18,37 @@ function startTimer() {
     timer = setInterval(function() {
         timerCount--;
         //modify some sort of display element to show the new timer value
-
+        timerEleValue.textContent = timerCount;
         //on lose
         if (timerCount <= 0){
             lose = true;
             // call loseGame which resets the timerCount and creates a reset button
-            // for the user to press to restart the game  
-            loseGame(); 
-        }
-        if ( win || lose){
+            // for the user to press to restart the game 
             clearInterval(timer);
+            timerCount = 60;
+            loseGame(); 
+            
+        }
+        if ( win || lose || reset){
+            clearInterval(timer);
+            timerCount = 60;
         }
     }, 1000);
 }
 
-//loseGame function
+function startGame() {
+    reset = false;
+    startButton.disabled = true;
+    resetButton.disabled = false;
+    startTimer();
+}
 
+function resetGame() {
+    reset = true;
+    startButton.disabled = false;
+    resetButton.disabled = true;
+
+}
 function loseGame() {
     timerCount = 60;
     lose = false;
@@ -38,6 +57,7 @@ function loseGame() {
     //change display element to show the reset button or something...
 }
 
+
 function winGame(){
     timerCount = 60;
     lose = false;
@@ -45,3 +65,7 @@ function winGame(){
     totalwins++;
     //change display element to show the reset button or something...
 }
+
+//start the game when pressing a button that says start.
+startButton.addEventListener("click",startGame);
+resetButton.addEventListener("click",resetGame);
